e40ba2436291f090d540f27f8d127f7634cef9ad
dobin <dobin@broken.ch>
2024-07-27 21:54:50 +0200

    build fix

--- create.bat
+++ create.bat
@@ -1,8 +1,8 @@
-sc create mydumbedr type=kernel binpath=Z:\windev\MyDumbEDR\x64\Debug\MyDumbEDRDriver.sys
+sc create mydumbedr type=kernel binpath=C:\Users\hacker\source\repos\mydumbedr\x64\Debug\MyDumbEDRDriver\MyDumbEDRDriver.sys
 sc start mydumbedr
-start cmd.exe /c Z:\windev\MyDumbEDR\x64\Debug\MyDumbEDRStaticAnalyzer.exe 
-start cmd.exe /c Z:\windev\MyDumbEDR\x64\Debug\MyDumbEDRRemoteInjector.exe 
-start cmd.exe /K "cd Z:\windev\MyDumbEDR\x64\Debug"
+start cmd.exe /c C:\Users\hacker\source\repos\mydumbedr\x64\Debug\MyDumbEDRStaticAnalyzer.exe 
+start cmd.exe /c C:\Users\hacker\source\repos\mydumbedr\x64\Debug\MyDumbEDRRemoteInjector.exe 
+REM start cmd.exe /K "cd C:\Users\hacker\source\repos\mydumbedr\x64\Debug"
 
 echo EDR's running, press any key to stop it
 pause
