fafa383fac9d8c05076fe65abeeb13ec710dad39
dobin <dobin@broken.ch>
2024-07-28 19:18:42 +0200

    remove injector start

--- create.bat
+++ create.bat
@@ -3,13 +3,13 @@ REM start cmd.exe /c C:\Users\hacker\source\repos\mydumbedr\x64\Debug\MyDumbEDRS
 sc create mydumbedr type=kernel binpath=C:\Users\hacker\source\repos\mydumbedr\x64\Debug\MyDumbEDRDriver\MyDumbEDRDriver.sys
 sc start mydumbedr
 
-start cmd.exe /c C:\Users\hacker\source\repos\mydumbedr\x64\Debug\MyDumbEDRRemoteInjector.exe 
+REM start cmd.exe /c C:\Users\hacker\source\repos\mydumbedr\x64\Debug\MyDumbEDRRemoteInjector.exe 
 REM start cmd.exe /K "cd C:\Users\hacker\source\repos\mydumbedr\x64\Debug"
 
 echo EDR's running, press any key to stop it
 pause
 
 REM taskkill /F /IM MyDumbEDRStaticAnalyzer.exe 
-taskkill /F /IM MyDumbEDRRemoteInjector.exe
+REM taskkill /F /IM MyDumbEDRRemoteInjector.exe
 sc stop mydumbedr
 sc delete mydumbedr
