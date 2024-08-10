b9d4526bff54e4dae4562acf1d21caa932201f96
dobin <dobin@broken.ch>
2024-08-10 20:54:21 +0200

    tmp

--- MyDumbEDRRemoteInjector/MyDumbEDRRemoteInjector.cpp
+++ MyDumbEDRRemoteInjector/MyDumbEDRRemoteInjector.cpp
@@ -4,16 +4,69 @@
 #define MESSAGE_SIZE 2048
 #define MAX_PATH 260
 
-int main() {
+char dll_full_path[MAX_PATH];
+
+
+BOOL remote_inject(DWORD target_pid) {
+
+    // Opening the process with necessary privileges 
+    HANDLE hProcess = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ, FALSE, target_pid);
+    if (hProcess == NULL) {
+        printf("Can't open handle, error: % lu\n", GetLastError());
+        return FALSE;
+    }
+    printf("\tOpen handle on PID: %d\n", target_pid);
+
+    // Looking for the LoadLibraryA function in the kernel32.dll
+    FARPROC loadLibAddress = GetProcAddress(GetModuleHandle(L"kernel32.dll"), "LoadLibraryA");
+    if (loadLibAddress == NULL) {
+        printf("Could not find LoadLibraryA, error: %lu\n", GetLastError());
+        return FALSE;
+    }
+    printf("\tFound LoadLibraryA function\n");
+
+    // Allocating some RWX memory
+    LPVOID vae_buffer;
+    vae_buffer = VirtualAllocEx(hProcess, NULL, MAX_PATH, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    if (vae_buffer == NULL) {
+        printf("Can't allocate memory, error: %lu\n", GetLastError());
+        CloseHandle(hProcess);
+        return FALSE;
+    }
+    printf("\tAllocated: %d bytes\n", MAX_PATH);
+
+    // Writing the path of the DLL to inject x64\Debug\MyDumbEDRDLL
+    SIZE_T bytesWritten;
+    if (!WriteProcessMemory(hProcess, vae_buffer, dll_full_path, MAX_PATH, &bytesWritten)) {
+        printf("Can't write into memory, error: %lu\n", GetLastError());
+        VirtualFreeEx(hProcess, vae_buffer, MESSAGE_SIZE, MEM_RELEASE);
+        CloseHandle(hProcess);
+        return FALSE;
+    }
+    printf("\tWrote %zu in %d process memory\n", bytesWritten, target_pid);
+
+    // Creating a thread that will call LoadLibraryA and the path of the MyDUMBEDRDLL to load as argument
+    HANDLE hThread = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)loadLibAddress, vae_buffer, 0, NULL);
+    if (hThread == NULL) {
+        printf("Can't launch remote thread, error: %lu\n", GetLastError());
+        VirtualFreeEx(hProcess, vae_buffer, MESSAGE_SIZE, MEM_RELEASE);
+        CloseHandle(hProcess);
+        return FALSE;
+    }
+    printf("\tLaunched remote thread\n");
+
+    VirtualFreeEx(hProcess, vae_buffer, MESSAGE_SIZE, MEM_RELEASE);
+    CloseHandle(hThread);
+    CloseHandle(hProcess);
+    printf("\tClosed handle\n");
+
+    return TRUE;
+}
+
+
+BOOL piper() {
     LPCWSTR pipeName = L"\\\\.\\pipe\\dumbedr-injector";
     DWORD bytesRead = 0;
-    wchar_t target_binary_file[MESSAGE_SIZE] = { 0 };
-
-    char dll_path[] = "x64\\Debug\\MyDumbEDRDLL.dll";
-    char dll_full_path[MAX_PATH];
-    GetFullPathNameA(dll_path, MAX_PATH, dll_full_path, NULL);
-    printf("Launching injector named pipe server, injecting %s\n", dll_full_path);
-
 
     // Creates a named pipe
     HANDLE hServerPipe = CreateNamedPipe(
@@ -36,7 +89,7 @@ int main() {
         );
 
         wchar_t message[MESSAGE_SIZE] = { 0 };
-        
+
         if (isPipeConnected) {
 
             // Read from the named pipe
@@ -52,56 +105,8 @@ int main() {
             DWORD target_pid = _wtoi(message);
             printf("~> Received process id %d\n", target_pid);
 
-            // Opening the process with necessary privileges 
-            HANDLE hProcess = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ, FALSE, target_pid);
-            if (hProcess == NULL) {
-                printf("Can't open handle, error: % lu\n", GetLastError());
-                return FALSE;
-            }
-            printf("\tOpen handle on PID: %d\n", target_pid);
+            remote_inject(target_pid);
 
-            // Looking for the LoadLibraryA function in the kernel32.dll
-            FARPROC loadLibAddress = GetProcAddress(GetModuleHandle(L"kernel32.dll"), "LoadLibraryA");
-            if (loadLibAddress == NULL) {
-                printf("Could not find LoadLibraryA, error: %lu\n", GetLastError());
-                return FALSE;
-            }
-            printf("\tFound LoadLibraryA function\n");
-
-            // Allocating some RWX memory
-            LPVOID vae_buffer;
-            vae_buffer = VirtualAllocEx(hProcess, NULL, MAX_PATH, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-            if (vae_buffer == NULL){
-                printf("Can't allocate memory, error: %lu\n", GetLastError());
-                CloseHandle(hProcess);
-                return FALSE;
-            }
-            printf("\tAllocated: %d bytes\n", MAX_PATH);
-
-            // Writing the path of the DLL to inject x64\Debug\MyDumbEDRDLL
-            SIZE_T bytesWritten;
-            if (!WriteProcessMemory(hProcess, vae_buffer, dll_full_path, MAX_PATH, &bytesWritten)) {
-                printf("Can't write into memory, error: %lu\n", GetLastError());
-                VirtualFreeEx(hProcess, vae_buffer, MESSAGE_SIZE, MEM_RELEASE);
-                CloseHandle(hProcess);
-                return FALSE;
-            }
-            printf("\tWrote %zu in %d process memory\n", bytesWritten, target_pid);
-
-            // Creating a thread that will call LoadLibraryA and the path of the MyDUMBEDRDLL to load as argument
-            HANDLE hThread = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)loadLibAddress, vae_buffer, 0, NULL);
-            if (hThread == NULL) {
-                printf("Can't launch remote thread, error: %lu\n", GetLastError());
-                VirtualFreeEx(hProcess, vae_buffer, MESSAGE_SIZE, MEM_RELEASE);
-                CloseHandle(hProcess);
-                return FALSE;
-            }
-            printf("\tLaunched remote thread\n");
-
-            VirtualFreeEx(hProcess, vae_buffer, MESSAGE_SIZE, MEM_RELEASE);
-            CloseHandle(hThread);
-            CloseHandle(hProcess);
-            printf("\tClosed handle\n");
 
             wchar_t response[MESSAGE_SIZE] = { 0 };
             swprintf_s(response, MESSAGE_SIZE, L"OK\0");
@@ -120,7 +125,20 @@ int main() {
                 hServerPipe // Handle to the named pipe
             );
 
-            printf("\n\n");        
+            printf("\n\n");
         }
     }
+}
+
+
+int main() {
+    char dll_path[] = "x64\\Debug\\MyDumbEDRDLL.dll";
+    wchar_t target_binary_file[MESSAGE_SIZE] = { 0 };
+
+    GetFullPathNameA(dll_path, MAX_PATH, dll_full_path, NULL);
+    printf("Launching injector named pipe server, injecting %s\n", dll_full_path);
+
+    DWORD target_pid = 7000;
+    printf("~> Received process id %d\n", target_pid);
+    remote_inject(target_pid);
 }
